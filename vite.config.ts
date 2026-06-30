9898f6e1bb2488e9bafccacad2d478181a187e77
Warm synths <warmsynthsiloveyou@gmail.com>
2026-06-30 15:55:18 +1000

    chore: update build output directory to docs and add node type definitions

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import { resolve } from 'path';
 export default defineConfig({
   base: './',
   build: {
-    target: 'es2022'
+    target: 'es2022',
+    outDir: 'docs'
   },
   server: {
     port: 3000,
